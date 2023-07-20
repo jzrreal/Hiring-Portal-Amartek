fc373d9046eb58b8cfd4949e9a54c3018e2ee911
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-20 18:36:39 +0700

    Add CandidateProfile controller, repository, service, and dto

---
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/dto/CandidateProfileRequest.java
@@ -0,0 +1,25 @@
+package com.hiringportal.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Builder
+public class CandidateProfileRequest {
+    @NotNull
+    private Integer id;
+    @NotBlank
+    private String phone;
+    private String summary;
+    @NotNull
+    private Date birthDate;
+    @NotNull
+    private Integer userId;
+}
--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/CandidateProfileRepository.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/CandidateProfileRepository.java
@@ -1,11 +1,26 @@
 package com.hiringportal.repository;
 
+import com.hiringportal.dto.CandidateProfileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hiringportal.entities.CandidateProfile;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface CandidateProfileRepository extends JpaRepository<CandidateProfile, Integer> {
-    
+    @Query(value = """
+            select new com.hiringportal.dto.CandidateProfileResponse(u.email, u.fullName, c.phone, c.summary, c.birthDate)
+            from CandidateProfile c
+            join User u on c.user.id = u.id
+            """)
+    List<CandidateProfileResponse> getAllCandidateProfile();
+    @Query(value = """
+            select new com.hiringportal.dto.CandidateProfileResponse(u.email, u.fullName, c.phone, c.summary, c.birthDate)
+            from CandidateProfile c
+            join User u on c.user.id = u.id where c.id = :id
+            """)
+    Optional<CandidateProfileResponse> getCandidateProfileById(Integer id);
 }
