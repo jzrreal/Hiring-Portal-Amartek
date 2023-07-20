9d7ef5ab1df3580e90991ff54d8b672adbc96577
Ichoko <richoattir@gmail.com>
2023-07-20 15:45:40 +0700

    any changes

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/UserRepository.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.hiringportal.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-    
+public interface UserRepository extends JpaRepository<User, Integer> { ;
 }
