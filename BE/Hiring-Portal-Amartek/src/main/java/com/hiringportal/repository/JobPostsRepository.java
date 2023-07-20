395c679a9c3c90dc4161722be46155a6318f91f7
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-20 10:49:13 +0700

    Refactor repository name from prular to singular

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/JobPostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hiringportal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hiringportal.entities.JobPosts;
-
-public interface JobPostsRepository extends JpaRepository<JobPosts, Integer> {
-
-}
