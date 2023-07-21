5e443029465858b71c5e8dab94a0b038d6be0c37
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2023-07-21 16:41:21 +0700

    FE(Install Sweet Alert, View Login, View Register, View Not Found, View Master Data(Role, Candidate Profile, Skill Level, Qustion Level, Job Level, Job Function)) - 21 July 2023

---
+++ FE/hiring-portal/src/pages/human-resource/role/role.js
@@ -0,0 +1,12 @@
+import Swal from 'sweetalert2'
+
+function testAlert() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        confirmButtonText: 'Yes, Delete Now!'
+    })
+}
