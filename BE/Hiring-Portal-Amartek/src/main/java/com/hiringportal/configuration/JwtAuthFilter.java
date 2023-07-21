21be3bece0bb913d7bc30b7875209ebc7998dcef
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-21 17:04:05 +0700

    Add jwt service, jwt filter, and logout handler for jwt logout

---
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/JwtAuthFilter.java
@@ -0,0 +1,63 @@
+package com.hiringportal.configuration;
+
+import com.hiringportal.repository.TokenRepository;
+import com.hiringportal.service.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String userEmail;
+        String jwtToken;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authHeader.substring(7);
+        log.info("Token : {}", jwtToken);
+        userEmail = jwtService.extractUsername(jwtToken);
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+            Boolean isTokenValid = tokenRepository.findByToken(jwtToken)
+                    .map(token -> !token.getExpired() && !token.getRevokeToken())
+                    .orElse(false);
+
+            if (jwtService.isTokenValid(jwtToken, userDetails) && isTokenValid){
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
