d0ade20b928c50536df1a8b46a9bf11f267a9090
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-21 17:02:03 +0700

    Initiate AuthControllerTest

---
+++ BE/Hiring-Portal-Amartek/src/test/java/com/hiringportal/controller/AuthControllerTest.java
@@ -0,0 +1,23 @@
+package com.hiringportal.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+}
