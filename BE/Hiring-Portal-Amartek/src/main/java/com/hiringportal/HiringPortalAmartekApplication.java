e341b4860a62d249a78fdc97ef89e86ecf245549
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-22 00:04:18 +0700

    Add Async annotation to send email in different thread

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/HiringPortalAmartekApplication.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/HiringPortalAmartekApplication.java
@@ -2,8 +2,10 @@ package com.hiringportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HiringPortalAmartekApplication {
 
 	public static void main(String[] args) {
