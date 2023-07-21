8e04912f5d563de85651fc1d51701710e16c1c73
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-21 17:04:48 +0700

    Add jwt to filterchain

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
@@ -1,23 +1,42 @@
 package com.hiringportal.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutConfiguration logoutConfiguration;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
+                .antMatchers("/api/test").authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .csrf().disable();
-
+                .csrf().disable()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .logout()
+                .logoutUrl("/api/auth/logout")
+                .addLogoutHandler(logoutConfiguration)
+                .logoutSuccessHandler((request, response, authentication) ->
+                        SecurityContextHolder.clearContext());
         return http.build();
     }
 }
