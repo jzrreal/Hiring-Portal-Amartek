c008383454f19d204241ff28b034d736d588eb8c
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-22 17:10:48 +0700

    Add TestParameter model, controller, and unit test

---
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/TestParameterRepository.java
@@ -0,0 +1,7 @@
+package com.hiringportal.repository;
+
+import com.hiringportal.entities.TestParameter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestParameterRepository extends JpaRepository<TestParameter, Integer> {
+}
