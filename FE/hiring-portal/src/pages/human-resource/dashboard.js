b9df2e73d5b256bf2b7b33da231fdb8b9c997011
Jezriel Lukas L <jezriellukas@gmail.com>
2023-07-25 11:11:12 +0700

    Update dashboard.js
    created axios get for dashboard data

--- FE/hiring-portal/src/pages/human-resource/dashboard.js
+++ FE/hiring-portal/src/pages/human-resource/dashboard.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Navbar from "../../components/navbar";
 import Sidebar from "../../components/sidebar";
 import Footer from "../../components/footer";
 
 function Dashboard() {
+  const [totalJobs, setTotalJobs] = useState(0);
+  const [totalApplicants, setTotalApplicants] = useState(0);
+
+  useEffect( () => {
+    
+    axios({
+      method: "GET",
+      url: process.env.REACT_APP_API_URL + "/api/dashboards",
+      headers: {
+        Authorization : "Bearer " + localStorage.getItem("authToken")
+      }
+    })
+    .then(response => {
+      setTotalJobs(response.data.data.total_job_post)
+      setTotalApplicants(response.data.data.applicant_responses.length)
+    })
+    .catch(err => {
+      setTotalJobs(0);
+      setTotalApplicants(0);
+    })
+  }, []);
+
   return (
     <div className="wrapper">
       {/* Navbar */}
@@ -38,7 +61,7 @@ function Dashboard() {
                 <div className="info-box-content">
                   <span className="info-box-text">Total Jobs</span>
                   <span className="info-box-number">
-                    10
+                    {totalJobs}
                     <small>%</small>
                   </span>
                 </div>
@@ -49,7 +72,7 @@ function Dashboard() {
                 <span className="info-box-icon bg-danger elevation-1"><i className="fas fa-thumbs-up"></i></span>
                 <div className="info-box-content">
                   <span className="info-box-text">Total Applicants</span>
-                  <span className="info-box-number">41,410</span>
+                  <span className="info-box-number">{totalApplicants}</span>
                 </div>
               </div>
             </div>
@@ -59,7 +82,7 @@ function Dashboard() {
                 <span className="info-box-icon bg-success elevation-1"><i className="fas fa-shopping-cart"></i></span>
                 <div className="info-box-content">
                   <span className="info-box-text">Total Jobs</span>
-                  <span className="info-box-number">760</span>
+                  <span className="info-box-number">{totalJobs}</span>
                 </div>
               </div>
             </div>
@@ -68,7 +91,7 @@ function Dashboard() {
                 <span className="info-box-icon bg-warning elevation-1"><i className="fas fa-users"></i></span>
                 <div className="info-box-content">
                   <span className="info-box-text">Total Applicants</span>
-                  <span className="info-box-number">2,000</span>
+                  <span className="info-box-number">{totalApplicants}</span>
                 </div>
               </div>
             </div>
