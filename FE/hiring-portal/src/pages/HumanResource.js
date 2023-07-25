72f5d7a46ad4b7f1f6b3a37bb23af0bc6e1e0fe2
Jezriel Lukas L <jezriellukas@gmail.com>
2023-07-25 13:20:28 +0700

    Update HumanResource.js

--- FE/hiring-portal/src/pages/HumanResource.js
+++ FE/hiring-portal/src/pages/HumanResource.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 export default function HumanResource () {
 
+    const [tokenValidation, setTokenValidation] = useState(false)
 
+    useEffect( () => {
+        axios({
+            method: "GET",
+            url: process.env.REACT_APP_API_URL + "/api/tokens",
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("authToken")
+            }
+        })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => console.log(err))
+    }, [])
 
     if(!localStorage.getItem("authToken")) {
         return <Navigate to="/login" />;
