591a5bbf53814b1c4d2adba3bf6dda836500d9a7
Jezriel Lukas L <jezriellukas@gmail.com>
2023-07-26 17:30:32 +0700

    Update emailVerification.js

--- FE/hiring-portal/src/pages/emailVerification.js
+++ FE/hiring-portal/src/pages/emailVerification.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function EmailVerification() {
 
   const token = new URLSearchParams(window.location.search).get("token")
 
   const [contentTitle, setContentTitle] = useState("")
+  const [email, setEmail] = useState("")
   
   useEffect(()=>{
     axios({
@@ -21,19 +23,63 @@ export default function EmailVerification() {
     })
   }, []);
 
-  return (
-    <div className="lockscreen-wrapper">
-      <div className="lockscreen-logo">
-        <img src="https://www.amartek.id/i/logo/weblogo-amartek.png" />
+  if (window.location.href === "http://localhost:3000/email-verification") {
+    return (
+      <div className="hold-transition login-page">
+          <div className="login-box">
+              <div className="login-logo">
+                  <img src="https://www.amartek.id/i/logo/weblogo-amartek.png" />
+              </div>
+              <div className="card mt-5">
+                  <div className="card-body login-card-body">
+                      <p className="login-box-msg">Sign In to start your session</p>
+
+                      <form method="POST" onSubmit="">
+                          <div className="form-group">
+                              <label for="exampleInputEmail1">Email</label>
+                              <div className="input-group mb-3">
+                                  <input 
+                                      type="email" 
+                                      value={email}
+                                      onChange={e => setEmail(e.target.value)}
+                                      className="form-control" 
+                                      placeholder="Email" />
+                                  <div className="input-group-append">
+                                      <div className="input-group-text">
+                                          <span className="fas fa-envelope"></span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+                          <a href="/register" className="btn btn-outline-primary btn-block">Register</a>
+                      </form>
+                      <div>
+                          <Link to="/register">Resend email verification?</Link>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-      <div className="help-block text-center my-5">
-        <h1 className="font-weight-bold">{contentTitle}</h1>
-        <a href={`http://localhost:3000/login`} className="btn btn-primary mt-3">Back to Login</a>
+    )
+  }
+
+  else {
+
+    return (
+      <div className="lockscreen-wrapper">
+        <div className="lockscreen-logo">
+          <img src="https://www.amartek.id/i/logo/weblogo-amartek.png" />
+        </div>
+        <div className="help-block text-center my-5">
+          <h1 className="font-weight-bold">{contentTitle}</h1>
+          <a href={`http://localhost:3000/login`} className="btn btn-primary mt-3">Back to Login</a>
+        </div>
+        <div className="lockscreen-footer text-center">
+          <strong>Copyright &copy; 2023 <a href="https://www.amartek.id/">Bumi Amartha Teknologi Mandiri</a></strong>
+          <div>All rights reserved.</div>
+        </div>
       </div>
-      <div className="lockscreen-footer text-center">
-        <strong>Copyright &copy; 2023 <a href="https://www.amartek.id/">Bumi Amartha Teknologi Mandiri</a></strong>
-        <div>All rights reserved.</div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
