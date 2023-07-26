b3e230492fd27c8f5535b66b3df3368c44762299
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-26 11:28:15 +0700

    Move to folder transactional

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/controller/transactional/EducationHistoriesController.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/controller/transactional/EducationHistoriesController.java
@@ -1,27 +1,34 @@
-package com.hiringportal.controller;
+package com.hiringportal.controller.transactional;
 
-import com.hiringportal.dto.*;
-import com.hiringportal.entities.*;
+import com.hiringportal.dto.CustomResponse;
+import com.hiringportal.dto.EducationHistoriesResponse;
+import com.hiringportal.dto.EducationHistoriesUpsert;
+import com.hiringportal.entities.CandidateProfile;
+import com.hiringportal.entities.EducationHistory;
+import com.hiringportal.entities.User;
 import com.hiringportal.repository.CandidateProfileRepository;
+import com.hiringportal.service.ValidationService;
 import com.hiringportal.service.educationHistory.EducationHistoriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.time.Year;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000/")
-@RequestMapping("api/education-history")
+@RequestMapping("api/education-histories")
 public class EducationHistoriesController {
     private final EducationHistoriesService educationHistoriesService;
     private final CandidateProfileRepository candidateProfileRepository;
+    private final ValidationService validationService;
+
     @GetMapping
     public ResponseEntity<Object> get() {
         List<EducationHistoriesResponse> educationHistoryList = educationHistoriesService.getEducationList();
@@ -29,8 +36,10 @@ public class EducationHistoriesController {
                 educationHistoryList);
     }
 
-    @GetMapping("/candidate/{candidateId}")
-    public ResponseEntity<Object> getByCandidate(@PathVariable Integer candidateId ) {
+    @GetMapping("/applicants/{candidateId}")
+    public ResponseEntity<Object> getByCandidate(@PathVariable Integer candidateId) {
+        candidateProfileRepository.findById(candidateId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Applicants with Id " + candidateId + " not found"));
         List<EducationHistoriesResponse> educationHistoryList = educationHistoriesService.getEducationListCandidate(candidateId);
         return CustomResponse.generateResponse("Data found with total amount : " + educationHistoryList.size(), HttpStatus.OK,
                 educationHistoryList);
@@ -41,10 +50,14 @@ public class EducationHistoriesController {
         EducationHistory educationHistory = educationHistoriesService.getById(historiesId);
         return CustomResponse.generateResponse("Data found", HttpStatus.OK, educationHistory);
     }
-    @PostMapping()
-    public ResponseEntity<Object> post(@RequestBody EducationHistoriesUpsert upsert){
 
-        Integer upsertCandidateId = upsert.getCandidateId();
+    @PostMapping()
+    public ResponseEntity<Object> post(@RequestBody EducationHistoriesUpsert upsert, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+
+        validationService.validate(upsert);
+
+        Integer upsertCandidateId = user.getCandidateProfile().getId();
         CandidateProfile candidateProfile = candidateProfileRepository.findById(upsertCandidateId).orElse(null);
 
         Date yearStart = new Date(upsert.getYearStart() - 1900, 0, 1); // Format: year - 1900, month (0-11), day
@@ -63,16 +76,23 @@ public class EducationHistoriesController {
         educationHistoriesService.save(educationHistory);
         return CustomResponse.generateResponse("Success save", HttpStatus.OK);
     }
-    @PutMapping("/{historiesId}")
-    public ResponseEntity<Object> put(@Valid @RequestBody EducationHistoriesUpsert upsert, @PathVariable Integer historiesId) {
 
-        Integer candidateId = upsert.getCandidateId();
+    @PutMapping("/{historiesId}")
+    public ResponseEntity<Object> put(@Valid @RequestBody EducationHistoriesUpsert upsert,
+                                      @PathVariable Integer historiesId,
+                                      Authentication authentication
+    ) {
+        User user = (User) authentication.getPrincipal();
+
+        validationService.validate(upsert);
+
+        Integer candidateId = user.getCandidateProfile().getId();
 
         CandidateProfile candidateProfile = candidateProfileRepository.findById(candidateId).orElse(null);
 
+        educationHistoriesService.getById(historiesId);
+
         upsert.setId(historiesId);
-
-
         EducationHistory educationHistory = new EducationHistory(
                 historiesId,
                 upsert.getLevel(),
