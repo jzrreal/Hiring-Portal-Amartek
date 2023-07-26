7f7062cf5c79a945d9237f3018609a51350d9017
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-26 19:10:02 +0700

    DTO response for get all question for online test

---
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/dto/QuestionTestResponse.java
@@ -0,0 +1,22 @@
+package com.hiringportal.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class QuestionTestResponse {
+    private Integer testQuestionId;
+    private Integer questionId;
+    private String question;
+    private List<ChoiceTestResponse> choices;
+}
