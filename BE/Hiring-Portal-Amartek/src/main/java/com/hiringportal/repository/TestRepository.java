dd50b37ca3051e6be513b01402833a7bffdd48e0
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-27 14:39:25 +0700

    Add method for schedule job for chcek expired test

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/TestRepository.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/repository/TestRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
-public interface TestRepository extends JpaRepository<Test,Integer> {
+public interface TestRepository extends JpaRepository<Test, Integer> {
 
     Test findFirstByJobApplication_Id(Integer jobApplicationId);
+
     @Modifying
     @Query(value = """
             delete from Test t where t.testId = :testId
@@ -17,4 +20,10 @@ public interface TestRepository extends JpaRepository<Test,Integer> {
     void deleteTestById(Integer testId);
 
     Optional<Test> findTestByTestToken(String token);
+
+    @Query(value = """
+            select t from Test t
+            where t.result is null and (t.endTest < :date or t.endAt < :date)
+            """)
+    List<Test> findAllExpiredTestAndResultIsNull(Date date);
 }
