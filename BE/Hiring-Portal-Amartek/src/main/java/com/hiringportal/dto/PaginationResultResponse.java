b9da958d591af9a0177ae91fc3db91f5716f5de5
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-28 21:44:08 +0700

    Add dto for pagination result only one data

---
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/dto/PaginationResultResponse.java
@@ -0,0 +1,22 @@
+package com.hiringportal.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PaginationResultResponse<T> {
+    private T result;
+    private Integer page;
+    private Integer elements;
+    private Integer currentPage;
+}
