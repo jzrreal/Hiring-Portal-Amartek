85fc792f11a1872dc584913d49520c6cb7c7fd13
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-07-28 18:17:37 +0700

    Add authorization based on role

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
@@ -25,15 +25,35 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .antMatchers("/api/test").authenticated()
-                .antMatchers("/api/profiles/**").authenticated()
-                .antMatchers("/api/dashboards").authenticated()
-                .antMatchers("/api/tokens").authenticated()
-                .antMatchers(HttpMethod.POST, "/api/job-posts").hasAnyAuthority("human resource")
-                .antMatchers("/api/applications/*").authenticated()
-                .antMatchers(HttpMethod.GET, "/api/applications").hasAnyAuthority("applicant")
-                .antMatchers(HttpMethod.POST, "/api/education-histories").hasAnyAuthority("applicant")
-                .antMatchers(HttpMethod.PUT, "/api/education-histories/*").hasAnyAuthority("applicant")
+                //Need authentication no matter what the roles
+                .antMatchers("/api/dashboards", "/api/profiles", "/api/tokens").authenticated()
+                .antMatchers(HttpMethod.GET,
+                        "/api/job-posts",
+                        "/api/job-posts/*",
+                        "/api/education-histories/applicants/*",
+                        "/api/education-histories",
+                        "/api/job-functions",
+                        "/api/skill-levels",
+                        "/api/job-levels"
+                ).authenticated()
+                //Need authentication with role Candidate
+                .antMatchers(HttpMethod.POST, "/api/applications/*", "/api/education-histories").hasAuthority("applicant")
+                .antMatchers(HttpMethod.PUT, "/api/education-histories/*").hasAuthority("applicant")
+                .antMatchers(HttpMethod.DELETE, "/api/applications/*", "/api/education-histories/*").hasAuthority("applicant")
+                .antMatchers(HttpMethod.GET, "/api/applications", "/api/profiles/applicants").hasAuthority("applicant")
+                //Need authentication with role HR
+                .antMatchers(HttpMethod.POST, "/api/job-posts", "/api/online-tests/applications/*", "api/job-functions", "/api/skill-levels", "/api/question-levels", "/api/job-levels").hasAuthority("human resource")
+                .antMatchers(HttpMethod.PUT, "/api/job-posts/*", "api/job-functions/*", "/api/skill-levels/*", "/api/question-levels/*", "/api/job-levels/*").hasAuthority("human resource")
+                .antMatchers(HttpMethod.DELETE, "/api/job-posts/*", "api/job-functions/*", "/api/skill-levels/*", "/api/question-levels/*", "/api/job-levels/*").hasAuthority("human resource")
+                .antMatchers(HttpMethod.GET, "/api/applications/job-post/*", "/api/applications/*/applicants", "api/job-functions/*", "/api/skill-levels/*", "/api/question-levels/*", "/api/job-levels/*").hasAuthority("human resource")
+                .antMatchers("/api/application-status/**", "/api/roles/**", "api/test-parameters/**", "/api/applicants/**","/api/users/**").hasAuthority("human resource")
+                //Need authentication with role Trainer
+                .antMatchers(HttpMethod.POST, "/api/questions").hasAuthority("trainer")
+                .antMatchers(HttpMethod.PUT, "/api/questions/*").hasAuthority("trainer")
+                .antMatchers(HttpMethod.DELETE, "/api/questions/*").hasAuthority("trainer")
+                .antMatchers(HttpMethod.GET, "/api/questions", "/api/questions/*").hasAuthority("trainer")
+                //Trainer and HR
+                .antMatchers(HttpMethod.GET, "/api/question-levels").hasAnyAuthority("trainer", "human resource")
                 .anyRequest().permitAll()
                 .and()
                 .csrf().disable()
