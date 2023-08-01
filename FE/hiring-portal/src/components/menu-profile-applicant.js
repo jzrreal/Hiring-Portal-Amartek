68ad7857dde3bc63b374489e6b75f08ebd6a993e
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2023-08-02 02:02:50 +0700

    Change Icon

--- FE/hiring-portal/src/components/menu-profile-applicant.js
+++ FE/hiring-portal/src/components/menu-profile-applicant.js
@@ -1,26 +1,26 @@
 import { React } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function MenuApplicantProfile() {
     return (
         <ul className="nav flex-column">
             <li className="nav-item pt-0 pb-3">
-                <Link to="/applicant/profile/personal-information" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Personal Information</Link>
+                <NavLink to="/applicant/profile/personal-information" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Personal Information</NavLink>
             </li>
             <li className="nav-item py-3">
-                <Link to="/applicant/profile/education-histories" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Education Histories</Link>
+                <NavLink to="/applicant/profile/education-histories" className="nav-link text-muted px-0"><i className="fas fa-graduation-cap mr-2"></i> Education Histories</NavLink>
             </li>
             <li className="nav-item py-3">
-                <Link to="/applicant/profile/skills" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Skills</Link>
+                <NavLink to="/applicant/profile/skills" className="nav-link text-muted px-0"><i className="fas fa-rocket mr-2"></i> Skills</NavLink>
             </li>
             <li className="nav-item py-3">
-                <Link to="/applicant/profile/work-experiences" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Work Experiences</Link>
+                <NavLink to="/applicant/profile/work-experiences" className="nav-link text-muted px-0"><i className="fas fa-briefcase mr-2"></i> Work Experiences</NavLink>
             </li>
             <li className="nav-item py-3">
-                <Link to="/applicant/profile/certificates" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Certificates</Link>
+                <NavLink to="/applicant/profile/certificates" className="nav-link text-muted px-0"><i className="fas fa-certificate mr-2"></i> Certificates</NavLink>
             </li>
             <li className="nav-item py-3">
-                <Link to="/applicant/profile/achievments" className="nav-link text-muted px-0"><i className="fas fa-user mr-2"></i> Achievments</Link>
+                <NavLink to="/applicant/profile/achievments" className="nav-link text-muted px-0"><i className="fas fa-award mr-2"></i> Achievments</NavLink>
             </li>
         </ul>
     )
