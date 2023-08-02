c318f5265456bf95ebc6d9c489a151e482bab8d5
ahmadsgr39 <ahmadsgr39@gmail.com>
2023-08-02 16:44:59 +0700

    Refactor forbidden to unauthorized

--- BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
+++ BE/Hiring-Portal-Amartek/src/main/java/com/hiringportal/configuration/SecurityConfiguration.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -59,6 +61,8 @@ public class SecurityConfiguration {
                 .csrf().disable()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .logout()
